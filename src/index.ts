0e251c4738daf6c13a63342359ecb8338a4c079b
MallamTY <sosanyatemitayonurudeen@gmail.com>
2023-01-29 05:59:19 +0100

    Setting up for E-Commer API

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+
+import express, {Request, Response, NextFunction} from 'express';
+import { MONGO_URI, configType, PORT} from './accessories/configuration';
+import connectDB from './db/dbConnect';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('common'));
+app.use(express.json());
+
+
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({
+        message: err.message
+    })  
+});
+
+
+const startUp = async() => {
+    try {
+        const port: configType = PORT || 9000;
+        await connectDB(MONGO_URI)
+        app.listen(port,() => console.log(`\neCommerce-API running on port ${port}...`))
+                
+     } catch (error: any) {
+         console.log(`${error.message} was encountered while trying to connect to the database`);
+         process.exit(1)
+         
+     }
+     
+}
+
+startUp()
